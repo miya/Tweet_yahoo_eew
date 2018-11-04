19935b1754fd06b91176f09f4395dcf3db799576
m0zu <m0zurillex@gmail.com>
2018-11-05 01:06:03 +0900

    ツイートに座標情報を付与する機能を追加

--- tweet_yahoo_eew.py
+++ tweet_yahoo_eew.py
@@ -10,7 +10,7 @@ def main():
     info = [f[i].text for i in range(14) if i % 2 != 0]
     pic = soup.find(id='earthquake-01').find('img').get('src')
     check(pic)
-    text = '@test\n［地震速報］#jishin \n・時刻: {}\n・震源地: {}\n・最大震度: {}\n・マグニチュード: {}\n・深さ: {}\n・緯度/経度: {}\n・情報: {}\n'.format(info[0],info[1],info[2],info[3],info[4],info[5],info[6])
+    text = '［地震速報］#jishin \n・時刻: {}\n・震源地: {}\n・最大震度: {}\n・マグニチュード: {}\n・深さ: {}\n・緯度/経度: {}\n・情報: {}\n'.format(info[0],info[1],info[2],info[3],info[4],info[5],info[6])
     geocode = [i for i in re.findall('\d*[.,]?\d*',info[5]) if i != '']
     get_pic(pic)
     tweet(text,geocode[0],geocode[1])
