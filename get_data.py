2a8b20413c908fa5d3228881778a3311ce91a8ab
m0zu <m0zurillex@gmail.com>
2018-11-12 23:04:09 +0900

    軽微な修正

--- get_data.py
+++ get_data.py
@@ -9,7 +9,7 @@ def data():
     pic = soup.find(id='earthquake-01').find('img').get('src')
     check(pic)
     get_pic(pic)
-    text = '@test\n［test］\n・時刻: {}\n・震源地: {}\n・最大震度: {}\n・マグニチュード: {}\n・深さ: {}\n・緯度/経度: {}\n・情報: {}'.format(info[0],info[1],info[2],info[3],info[4],info[5],info[6])
+    text = '［地震速報］\n・時刻: {}\n・震源地: {}\n・最大震度: {}\n・マグニチュード: {}\n・深さ: {}\n・緯度/経度: {}\n・情報: {}'.format(info[0],info[1],info[2],info[3],info[4],info[5],info[6])
     geocode = [i for i in re.findall('\d*[.,]?\d*',info[5]) if i != '']
     gal = [i.text for i in soup.find_all(width="10%")]
     place = soup.find_all(width="90%")
