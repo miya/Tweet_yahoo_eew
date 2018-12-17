adbe4399b2dfefc630d1ac7147b2ecd73b19894b
m0zu <m0zurillex@gmail.com>
2018-12-17 14:34:34 +0900

    軽微な修正

--- EEW.py
+++ EEW.py
@@ -6,11 +6,8 @@ from bs4 import BeautifulSoup
 from mastodon import Mastodon
 
 class EEW:
-    def init(self):
-        pass
-
     def get_data(self):
-        url = 'https://typhoon.yahoo.co.jp/weather/jp/earthquake/'
+        url = 'https://typhoon.yahoo.co.jp/weather/jp/earthquake/20110331161539.html'
         req = requests.get(url)
         soup = BeautifulSoup(req.text, 'html.parser')
         info = [i.text for i in soup.find_all(width="70%")]
@@ -21,13 +18,12 @@ class EEW:
         geocode = re.findall('\d+[.]+\d', info[5])
         place = soup.find_all(width="90%")[::-1]
         text2 = []
-        for i in range(int(info[2])):
-            p = '(.+?)\u3000'
-            m = re.findall(p, str(place[i]))
-            t = '、'.join(m)
-            if len(t) >= 133:
-                t = '{:.133}...'.format(t)
-            text2.append('《震度{}》 {}'.format(i+1, t))
+        intensity = int(re.findall('\d+', info[2])[0])
+        for i in range(intensity):
+            p = '、'.join(re.findall('(.+?)\u3000', str(place[i])))
+            if len(p) >= 133:
+                p = '{:.133}...'.format(p)
+            text2.append('《震度{}》 {}'.format(i+1, p))
         return text, text2, geocode
 
     def __get_image(self, image_url):
@@ -39,14 +35,14 @@ class EEW:
         with open('check.txt','r') as file1:
             t = file1.read()
         if text != t:
-            print('update')
+            print('更新があります')
             with open('check.txt','w') as file2:
                 file2.write(text)
         else:
-            print('no update')
+            print('更新がありません')
             sys.exit()
 
-    def tweet(self, text, lat=None, long_=None, id_=None):
+    def twitter_api(self):
         consumer_key = 'consumer_key'
         consumer_secret = 'consumer_secret'
         access_key = 'access_key'
@@ -54,22 +50,30 @@ class EEW:
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_key, access_secret)
         api = tweepy.API(auth)
+        return api
+
+    def tweet(self, text, lat=None, lon=None, id_=None):
+        api = self.twitter_api()
         if id_ == None:
-            tweet = api.update_with_media(filename='eew.png', status=text, lat=lat, long=long_)
+            tweet = api.update_with_media(filename='eew.png', status=text, lat=lat, long=lon)
             return tweet.id
-        if id_ != None:
+        elif id_ != None:
             tweet = api.update_status(status=text, in_reply_to_status_id =id_)
             return tweet.id
 
-    def toot(self, text, id_=None):
+    def mastodon_api(self):
         mastodon = Mastodon(
         client_id="app_key.txt",
         access_token="user_key.txt",
         api_base_url = "https://pawoo.net")
+        return mastodon
+
+    def toot(self, text, id_=None):
+        mastodon = self.mastodon_api()
         media_files = mastodon.media_post('eew.png', "image/png")
         if id_ == None:
-            toot = mastodon.status_post(status=text, visibility='public', media_ids=media_files)
+            toot = mastodon.status_post(status=text, visibility='direct', media_ids=media_files)
             return toot.id
-        if id_ != None:
-            toot = mastodon.status_post(status=text, visibility='public', in_reply_to_id=id_)
+        elif id_ != None:
+            toot = mastodon.status_post(status=text, visibility='direct', in_reply_to_id=id_)
             return toot.id
