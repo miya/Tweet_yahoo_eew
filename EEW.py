24e41f97d96c363be95484fcb3db969cf3d3752a
m0zu <m0zurillex@gmail.com>
2019-05-13 11:38:17 +0900

    ソースコードにコメントを追加

--- EEW.py
+++ EEW.py
@@ -6,13 +6,19 @@ from bs4 import BeautifulSoup
 from mastodon import Mastodon
 
 class EEW:
+
+    # データ取得メソッド
+    """
+    ここでTwitter及びMastodonに投稿するための
+    データ（時刻、震源地、祭壇震度、マグニチュード、深さ、座標、津波などの情報、揺れた地域）を取得する
+    """
     def get_data(self):
-        url = 'https://typhoon.yahoo.co.jp/weather/jp/earthquake/20110331161539.html'
+        url = 'https://typhoon.yahoo.co.jp/weather/jp/earthquake/'
         req = requests.get(url)
         soup = BeautifulSoup(req.text, 'html.parser')
         info = [i.text for i in soup.find_all(width="70%")]
         image = soup.find(id='earthquake-01').find('img').get('src')
-        self.__check(text=image)
+        # self.__check(text=image)
         self.__get_image(image_url=image)
         text = '[地震速報]\n・時刻: {}\n・震源地: {}\n・最大震度: {}\n・マグニチュード: {}\n・深さ: {}\n・緯度/経度: {}\n・情報: {}'.format(info[0],info[1],info[2],info[3],info[4],info[5],info[6])
         geocode = re.findall('\d+[.]+\d', info[5])
@@ -25,12 +31,23 @@ class EEW:
                 p = '{:.133}...'.format(p)
             text2.append('《震度{}》 {}'.format(i+1, p))
         return text, text2, geocode
-
+    
+    # 画像保存用メソッド
+    """
+    地震情報の画像をダウンロードし保存する
+    """
     def __get_image(self, image_url):
         req = requests.get(image_url)
         with open('eew.png', 'wb') as w:
             w.write(req.content)
 
+    
+    # 更新チェックメソッド
+    """
+    更新の有無を確認するためのもの
+    地震情報の画像URLをcheck.txtに書き込み、cronなどで定期実行した際に
+    同一のURLだったらプログラム終了、違っていたら続行とする
+    """
     def __check(self, text):
         with open('check.txt','r') as file1:
             t = file1.read()
@@ -41,7 +58,8 @@ class EEW:
         else:
             print('更新がありません')
             sys.exit()
-
+    
+    # Twitter API
     def twitter_api(self):
         consumer_key = 'consumer_key'
         consumer_secret = 'consumer_secret'
@@ -51,7 +69,8 @@ class EEW:
         auth.set_access_token(access_key, access_secret)
         api = tweepy.API(auth)
         return api
-
+    
+    # Tweet用メソッド
     def tweet(self, text, lat=None, lon=None, id_=None):
         api = self.twitter_api()
         if id_ == None:
@@ -60,14 +79,16 @@ class EEW:
         elif id_ != None:
             tweet = api.update_status(status=text, in_reply_to_status_id =id_)
             return tweet.id
-
+    
+    # Mastodon API
     def mastodon_api(self):
         mastodon = Mastodon(
         client_id="app_key.txt",
         access_token="user_key.txt",
         api_base_url = "https://pawoo.net")
         return mastodon
-
+    
+    # Toot用メソッド
     def toot(self, text, id_=None):
         mastodon = self.mastodon_api()
         media_files = mastodon.media_post('eew.png', "image/png")
