2a94273ca7c494aa085624dca6bce49e666c7b47
m0zu <m0zurillex@gmail.com>
2019-06-09 02:18:23 +0900

    sample.pyの削除

--- sample.py
+++
@@ -1,9 +0,0 @@
-from EEW import EEW
-
-eew = EEW()
-data = eew.get_data()
-
-tweet_id = eew.tweet(text=data[0], lat=data[2][0], lon=data[2][1])
-
-for i in data[1]:
-    tweet_id = eew.tweet(text=i, id_=tweet_id)
